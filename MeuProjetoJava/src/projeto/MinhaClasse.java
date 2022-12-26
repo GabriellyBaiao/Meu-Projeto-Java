ece702f91af4f29ebadfc1cf8d60ac14cf2b8c04
Gabrielly Baião <gabriellybaiao95@gmail.com>
2022-12-26 20:30:50 -0300

    commit initial

---
+++ MeuProjetoJava/src/projeto/MinhaClasse.java
@@ -0,0 +1,17 @@
+package projeto;
+
+import java.util.Scanner;
+
+public class MinhaClasse {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Digite um valor inteiro: ");
+		int valor;
+		valor = teclado.nextInt();
+		System.out.println("Valor digitado: " + valor);
+		
+
+	}
+
+}
